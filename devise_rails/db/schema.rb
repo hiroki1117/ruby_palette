08c68efee1ea19f0cf71868a186da109730a16b9
hiroki1117 <scala1117@gmail.com>
2025-11-09 18:08:59 +0900

    add task

--- devise_rails/db/schema.rb
+++ devise_rails/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_09_082842) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_09_085520) do
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "body"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,4 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_09_082842) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "tasks", "users"
 end
