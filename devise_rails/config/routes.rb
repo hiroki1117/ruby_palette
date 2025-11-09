efd506bdba951479d75716b44e80a70e490c6f03
hiroki1117 <scala1117@gmail.com>
2025-11-09 17:49:14 +0900

    add users_controller

--- devise_rails/config/routes.rb
+++ devise_rails/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :users, only: [:index]
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
